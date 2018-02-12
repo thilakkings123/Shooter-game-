00ba7ff4fb6d8ededaed829bcf71bd4b12a3509b
LAPTOP-1ARE4SOF\Thilak Raj <sound001>
2018-02-12 08:21:56 -0800

    first commit

---
+++ Assets/Scripts/Mover1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover1 : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+		
+	}
+	
+
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+	public float tumble;
+
+	void Start ()
+
+	{
+		Rigidbody rb;
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble; 
+	}
+}
